ab069541edab9e81b2841261ab32cf9c3c387c7d
Rıdvan Ekinci <ridvanekinci92@gmail.com>
2022-05-24 02:22:17 +0300

    update

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gamePaused = false;
+
+    public GameObject PauseMenuObject;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gamePaused == true)
+            {
+                ContinueGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ContinueGame()
+    {
+        PauseMenuObject.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    public void PauseGame()
+    {
+        PauseMenuObject.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Please Wait! Menu is Loading");
+    }
+}
